a5d20e4b5af647044792bcba90a5248d9a553536
Antonio Juan <145511918+ntoniojuan@users.noreply.github.com>
2026-04-01 06:53:10 +0800

    Type nextConfig as NextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // We will assume your GitHub repo will be named "my-portfolio"
   basePath: '/my-portfolio',
 };
 
