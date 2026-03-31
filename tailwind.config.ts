a53669fc1911d7e7a2e18ef3c16b3abe7883d19f
Antonio Juan <145511918+ntoniojuan@users.noreply.github.com>
2026-04-01 06:57:36 +0800

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "#f1f5f9",
+        foreground: "#1e293b",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
